8fe000bfbe33220b66ced13ec92a1dc5a3213e7a
unknown <nithishrajapuram31@gmail.com>
2020-06-06 13:55:21 +0530

    total commit

--- app.js
+++ app.js
@@ -165,6 +165,11 @@ app.post("/register",function(req,res){
      }
    });
  });
+ let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
 
  app.listen(3000, function() {
